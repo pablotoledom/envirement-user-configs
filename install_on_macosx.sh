ebfa81dd7c6506d2bed2b6559c199ed22920f07b
Jonathan Toledo Moya <pablotoledomoya@gmail.com>
2023-08-07 22:51:33 -0400

    update scripts

--- install_on_macosx.sh
+++ install_on_macosx.sh
@@ -19,10 +19,10 @@ echo 'Copying Nerd Fonts...'
 source ./show/divbar
 sleep .5
 
-# cp ./fonts/hack/*.* ~/Library/Fonts
-# cp ./fonts/meslogs/*.* ~/Library/Fonts
-# cp ./fonts/powerline/*.* ~/Library/Fonts
-# sleep .5
+cp ./fonts/hack/*.* ~/Library/Fonts
+cp ./fonts/meslogs/*.* ~/Library/Fonts
+cp ./fonts/powerline/*.* ~/Library/Fonts
+
 
 # *************************************************************** #
 #                    Install terminal tools                       #
@@ -37,17 +37,17 @@ mv -f ~/.zshrc ~/.zshrc.back
 mv -f ~/.vimrc ~/.vimrc.back
 
 # 2- Install software with Brew
-# echo ''
-# echo 'Instaling HomeBrew...'
-# sleep .5
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo ''
+echo 'Instaling HomeBrew...'
+sleep .5
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# brew install 7zip
-# brew install findutils
-# brew install zsh
-# brew install lsd
-# brew install bat
-# brew install tmux
+brew install 7zip
+brew install findutils
+brew install zsh
+brew install lsd
+brew install bat
+brew install tmux
 
 mkdir -p ~/tmp
 alias updatedb="gupdatedb --localpaths='/Users/${user}' --output='/Users/${user}/tmp/locatedb'"
@@ -61,18 +61,16 @@ echo ''
 sleep .5
 
 # 3- Copy ZSH plugins to /usr/share/zsh/plugins directory
-# sudo mkdir -p /usr/share/zsh/plugins/zsh-sudo
-# sudo cp -r copy_to_usr_share/zsh/plugins/zsh-sudo/sudo.plugin.zsh /usr/share/zsh/plugins/zsh-sudo/ # Add sudo word when press two times the return key
-# sudo cp -r ./copy_to_usr_share/zsh/plugins/zsh-autosuggestions /usr/share/zsh/plugins/ # v0.7.0
-# sudo cp -r ./copy_to_usr_share/zsh/plugins/zsh-syntax-highlighting /usr/share/zsh/plugins/ #v0.7.1
-
-# Copy to /usr/share directory
 sudo -S mkdir -p /usr/share/zsh/plugins
 sudo -S cp -r ./copy_to_usr_share/zsh/plugins/zsh-sudo /usr/share/zsh/plugins/ # Add sudo word when press two times the return key
 sudo -S cp -r ./copy_to_usr_share/zsh/plugins/zsh-autosuggestions /usr/share/zsh/plugins/ # v0.7.0
 sudo -S cp -r ./copy_to_usr_share/zsh/plugins/zsh-syntax-highlighting /usr/share/zsh/plugins/ #v0.7.1
 
 # 4- Copy neovim configuration
+echo ''
+echo 'Instaling NeoVim plugins'
+echo ''
+sleep .5
 cp -r ./copy_to_user_folder/.vimrc ~/
 cp -r ./copy_to_user_folder/.vim ~/
 
@@ -80,13 +78,13 @@ mkdir -p ~/.config/nvim
 cp -r ./copy_to_user_folder/.config/nvim/init.vim ~/.config/nvim/init.vim
 
 #  - Install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #  - Copy nvim plugins
 cd ./copy_to_user_folder/.vim/
 tar -zxvf plugged.tar.gz
-sudo cp -rf ./plugged/* ~/.vim/plugged
+yes | sudo cp -r ./plugged/* ~/.vim/plugged
 rm -R plugged
 cd ../..
 
@@ -109,4 +107,3 @@ rm ./copy_to_user_folder/zsh/themes/v1.19.0.tar.gz
 cp ./copy_to_user_folder/.zshrc ~/.zshrc
 
 # Change shell to user pablo
-# sudo usermod -S --shell /usr/bin/zsh $pablo
